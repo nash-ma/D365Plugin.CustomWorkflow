ed606a4379f474543eb0f3f09e9f4df2a26cc0d5
Nash Ma <mayanchao@outlook.jp>
2021-08-27 22:23:05 +0900

    クラス名の変更とSDKのインストール

--- D365Plugin.CustomWorkflow/AutoCount.cs
+++ D365Plugin.CustomWorkflow/AutoCount.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
 
 namespace D365Plugin.CustomWorkflow
 {
-    public class Class1
+    public class AutoCount
     {
     }
 }
